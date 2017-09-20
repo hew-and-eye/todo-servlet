74992ecbed9451e1848d4e94b1fbcec4536ef739
hew-and-eye <matthew.r.bernardo@gmail.com>
2017-09-20 13:25:17 -0400

    removed MySQL login info from the DUUP file. I should probably figure out how to delete old revisions, because the login info is still accessbile :/

--- TodoWebApp/src/jdbc/DUUP.java
+++ TodoWebApp/src/jdbc/DUUP.java
@@ -3,11 +3,7 @@ package jdbc;
 public class DUUP {
 	public static final String driver = "com.mysql.jdbc.Driver";
 	// replace with the hostgator parameters later
-	public static final String url = "jdbc:mysql://108.167.189.57:3306/mattbern_todo_db";
-	public static final String user = "mattbern_todo";
-	public static final String password = "2B=_X86cewFh";
-	/*
-	 * mattbern_todo_db 108.167.189.57 mattbern_todo
-	 * thisisanabsurdlylongpasswordforsuchatrivialthing
-	 */
+	public static final String url = "jdbc:mysql://fake.ip.address:3306/db_name";
+	public static final String user = "user_name";
+	public static final String password = "Oh hey, I actually remembered to delete the password from the github repo";
 }
